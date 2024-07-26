7676b39286fb97546f143711b87b16cefbca7c02
Kevin Uduji <checkupapp1@gmail.com>
2024-07-26 10:27:10 -0500

    Navbar Changes
    Co-Authored-By: Kevin Uduji <118309007+kevinuduji@users.noreply.github.com>

--- check-up-app/src/App.jsx
+++ check-up-app/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
     </>
   )
 }
--- check-up-app/src/main.jsx
+++ check-up-app/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Payment from './Pages/Payment.jsx'
 import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/setting" element={<App />} />
+        <Route path="/contact" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
